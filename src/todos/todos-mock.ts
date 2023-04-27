b7d52c622be4d21326f57813e2673dd3896f4bff
GOLDER303 <szymonstepien@interia.pl>
2023-04-27 20:30:48 +0200

    Add todos mock data

---
+++ src/todos/todos-mock.ts
@@ -0,0 +1,7 @@
+import { TodoDTO } from './todo.dto';
+
+export const todosData: TodoDTO[] = [
+  { id: 1, title: 'Buy groceries', status: 'done' },
+  { id: 2, title: 'Call grandma', status: 'inProgress' },
+  { id: 3, title: 'Finish project raport', status: 'todo' },
+];
