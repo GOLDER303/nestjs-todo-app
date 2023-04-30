5e26eb54a975854837c6cd9ede6347878467f7f7
GOLDER303 <szymonstepien@interia.pl>
2023-04-30 12:00:36 +0200

    Add todos service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { TodosController } from './todos/todos.controller';
+import { TodosService } from './todos/todos.service';
 
 @Module({
   imports: [],
   controllers: [TodosController],
-  providers: [PrismaService],
+  providers: [PrismaService, TodosService],
 })
 export class AppModule {}
