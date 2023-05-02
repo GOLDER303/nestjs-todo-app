5c6067e2af9b47c99e4736bf3638a08de19d3770
GOLDER303 <szymonstepien@interia.pl>
2023-05-02 11:25:33 +0200

    Create auth module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule, PrismaModule],
+  imports: [TodosModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
